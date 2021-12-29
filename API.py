b01816f911fb875b06e61c3ebfd16c251611dc36
sherali17 <45357972+sherali17@users.noreply.github.com>
2021-12-29 03:04:30 +0100

    Update API.py

--- API.py
+++ API.py
@@ -351,6 +351,7 @@ def entrainement_knn(df):
     return knn
 
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
    # app.run()
    # app.run(host="localhost", port="5000", debug=True)
+    app.run(debug=True)
