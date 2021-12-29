5c910c2a9ca53a7a2c3413928a3353308ba6f8bc
sherali17 <45357972+sherali17@users.noreply.github.com>
2021-12-29 02:24:39 +0100

    Update API.py
    desactivation app.run()

--- API.py
+++ API.py
@@ -351,6 +351,6 @@ def entrainement_knn(df):
     return knn
 
 
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+    "app.run()
    # app.run(host="localhost", port="5000", debug=True)
